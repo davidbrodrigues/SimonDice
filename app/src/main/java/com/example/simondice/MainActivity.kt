9d2ff516d5016c649650edb524f95de13ec8f392
dbragarodrigues <dbragarodrigues@danielcastelao.org>
2022-10-17 10:46:56 +0200

    Empezando

--- app/src/main/java/com/example/simondice/MainActivity.kt
+++ app/src/main/java/com/example/simondice/MainActivity.kt
@@ -3,12 +3,27 @@ package com.example.simondice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
+        var secuencia = ArrayList<String>()
+        var ronda = secuencia.size
+        var record = 0
+        var velocidad = 1
+
+
+    }
+
+
+    private fun ClickInicio(){
+        bstart.setOnClickListener{
+            ronda=1
+            velocidad=1
+        }
 
     }
 }
